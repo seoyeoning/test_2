f535862c3166c55a96d2224d782c6caf2cfc9869
seoyeoning <minalunpark@naver.com>
2023-08-10 20:52:16 +0900

    dd

---
+++ backend/src/main/java/hongsam/demo/board/controller/BoardController.java
@@ -0,0 +1,30 @@
+package hongsam.demo.board.controller;
+
+import hongsam.demo.board.repository.BoardUpdateDto;
+import hongsam.demo.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping
+    public String home() {
+        return "성공";
+    }
+
+    @PatchMapping("/{boardId}")
+    public void updateBoard(@PathVariable Long boardId, @RequestBody BoardUpdateDto boardUpdateDto) {
+        boardService.updateBoard(boardId, boardUpdateDto);
+    }
+
+    @DeleteMapping("/{boardId}")
+    public void deleteBoard(@PathVariable Long boardId) {
+        boardService.deleteBoard(boardId);
+    }
+
+}
