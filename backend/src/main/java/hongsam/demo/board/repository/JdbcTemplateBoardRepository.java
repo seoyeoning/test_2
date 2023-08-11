6651e3aea4b614f1388e09a462ef3fcc7677d451
seoyeoning <minalunpark@naver.com>
2023-08-11 11:36:33 +0900

    sql boardId 수정

--- backend/src/main/java/hongsam/demo/board/repository/JdbcTemplateBoardRepository.java
+++ backend/src/main/java/hongsam/demo/board/repository/JdbcTemplateBoardRepository.java
@@ -17,7 +17,7 @@ public class JdbcTemplateBoardRepository implements BoardRepository{
     @Override
     public void updateBoard(Long boardId, BoardUpdateDto updateParam) {
 
-        String sql = "update board set title = ?, content = ? where id = ?";
+        String sql = "update board set title = ?, content = ? where boardId = ?";
 
         template.update(sql,
                 updateParam.getTitle(),
@@ -28,7 +28,7 @@ public class JdbcTemplateBoardRepository implements BoardRepository{
     @Override
     public void deleteBoard(Long boardId) {
 
-        String sql = "delete from board where id = ?";
+        String sql = "delete from board where boardId = ?";
 
         template.update(sql, boardId);
     }
